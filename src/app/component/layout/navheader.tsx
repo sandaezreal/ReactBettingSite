a233913326ae35f7d2383b07e342903fa085461d
vvalencia <vvalencia@a1enterprise.com>
2024-04-28 06:23:31 +0800

    nav link for server name

--- src/app/component/layout/navheader.tsx
+++ src/app/component/layout/navheader.tsx
@@ -29,11 +29,11 @@ export function NavHeader() {
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
-        <Navbar.Link href="/ReactBettingSite" active>
+        <Navbar.Link href="ReactBettingSite" active>
           Home
         </Navbar.Link>
-        <Navbar.Link href="/ReactBettingSite/EsportBetting">Esport Beting</Navbar.Link>
-        <Navbar.Link href="/ReactBettingSite/SportsBetting">Sport Betting</Navbar.Link>
+        <Navbar.Link href="ReactBettingSite/EsportBetting">Esport Beting</Navbar.Link>
+        <Navbar.Link href="ReactBettingSite/SportsBetting">Sport Betting</Navbar.Link>
         <Navbar.Link href="#">Casino</Navbar.Link>
         <Navbar.Link href="#">Promo</Navbar.Link>
         <Navbar.Link href="#">Help</Navbar.Link>
