5bfa0b1c4ac8606a13b684c46f65f37dee5796a7
vvalencia <vvalencia@a1enterprise.com>
2024-05-01 00:31:18 +0800

    adding realdata in gamelist

---
+++ api/Esportapi.ts
@@ -0,0 +1,19 @@
+const axios = require('axios');
+
+const options = {
+  method: 'GET',
+  url: 'https://esportapi1.p.rapidapi.com/api/esport/matches/18/12/2022',
+  headers: {
+    'X-RapidAPI-Key': '2de9ab751bmsh5d6ab9b509cd6d2p1bede8jsnb47290082409',
+    'X-RapidAPI-Host': 'esportapi1.p.rapidapi.com'
+  }
+};
+
+export const MatchSchedules = async () => {
+try {
+	const response = await axios.request(options);
+	console.log(response.data);
+} catch (error) {
+	console.error(error);
+}
+};
